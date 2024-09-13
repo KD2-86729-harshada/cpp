57a8fc5fb52f432e8f4e4a2baed6ec5fb2db67a3
86729-harshada <harshada3678@gmail.com>
2024-09-13 17:40:03 +0530

    Assign3_3 updated

--- assign3_3.cpp
+++ assign3_3.cpp
@@ -10,7 +10,10 @@ class Address
 public:
     Address()
     {
-        
+        building = " ";
+        street = " ";
+        city = " ";
+        pin = 0;
     }
 
     void setBuilding(string building)
@@ -24,36 +27,64 @@ public:
 
     void setStreet(string street)
     {
-        //cout << "street : "<<endl;
         this->street = street;
     }
-
     string getStreet()
     {
         return street;
     }
 
+    void setCity(string city)
+    {
+        this->city = city;
+    }
+    string getCity()
+    {
+        return city;
+    }
+
+    void setPin(int pin)
+    {
+        this->pin = pin;
+    }
+
+    int getPin()
+    {
+        return pin;
+    }
     void acceptAddress()
     {
         cout << "Enter your Address" << endl;
-        cout<<"--------------------------------"<<endl;
-        // getline(cin,building);  
+        cout << "------------------------------------" << endl;
+        cout << "Enter the building :";
+        cin >> building;
+        cout << "enter the street :";
+        cin >> street;
+        cout << "enter the city :";
+        cin >> city;
+        cout << "enter the pin :";
+        cin >> pin;
+        // getline(cin,building);
     }
     void displayAddress()
     {
-        cout << "Street : " << getStreet()<< endl;
-        cout << "buildings : "<< getBuilding()<<endl;
+        cout << "buildings : " << getBuilding() << endl;
+        cout << "Street : " << getStreet() << endl;
+        cout << "City : " << getCity() << endl;
+        cout << "Pin : " << getPin() << endl;
     }
 };
 int main()
 {
     Address a1;
-    a1.acceptAddress();
 
-    a1.setBuilding("Anuda");
+    a1.setBuilding("ShriNivas");
     a1.setStreet("rankala");
+    a1.setCity("Kolhapur");
+    a1.setPin(416205);
 
     a1.displayAddress();
+    a1.acceptAddress();
 
     return 0;
 }
