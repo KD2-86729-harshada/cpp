0b97180266a30d8ec456c5a00fbcbcb3ce0888bc
86729-harshada <harshada3678@gmail.com>
2024-09-17 22:00:41 +0530

    updated

--- assign5_2.cpp
+++ assign5_2.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class student
 {
@@ -6,57 +6,80 @@ class student
     string name;
     string gender;
     static int genrollno;
+    int marks[3];
+    int obtainedMarks;
+    double percentage;
 
-public: 
-    student():rollNumber(++genrollno)
+    void calPercentage()
     {
-        name=" ";
-        gender= " ";
-        //rollNumber = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            obtainedMarks += marks[i];
+        }
+        percentage = (obtainedMarks / 300) * 100;
     }
-    student(string name, string gender, int rollNumber):rollNumber(++genrollno)
+
+public:
+    student() : rollNumber(++genrollno)
     {
-        this->name=name;
-        this->gender=gender;
+        name = " ";
+        gender = " ";
+        // rollNumber = 0;
+    }
+    student(string name, string gender, int rollNumber, int marks[3]) : rollNumber(++genrollno)
+    {
+        this->name = name;
+        this->gender = gender;
+        this->marks[3] = marks[3];
     }
 
     void acceptStudent()
     {
-        cout<<"roll number = "<<rollNumber<<endl;
-        cout<<"name = ";
-        cin>>name;
-        cout<<"gender = ";
-        cin>>gender;
+        cout << "roll number = " << rollNumber << endl;
+        cout << "name = ";
+        cin >> name;
+        cout << "gender = ";
+        cin >> gender;
+       
+        for (int i = 0; i < 3; i++)
+        {
+             cout << "subject "<<i+1<<":";
+            cin >> marks[i];
+        }
+        calPercentage();
     }
     void printStudent()
     {
-        cout<<"rollNumber = "<<rollNumber<<endl;
-        cout<<"name = "<<name<<endl;
-        cout<<"gender = "<<gender<<endl<<endl;
+        cout << "rollNumber = " << rollNumber << endl;
+        cout << "name = " << name << endl;
+        cout << "gender = " << gender << endl;
+        cout << "percentage : " << percentage << endl;
+        cout << "-------------------------------------" << endl;
     }
 };
 
-int student :: genrollno = 101;
+int student ::genrollno = 101;
 int main()
 {
     // student s1;
     // s1.printStudent();
-    student*arr[5];
-    arr[0]=new student();
-    arr[1]=new student();
-    arr[2]=new student();
-    arr[3]=new student();
-    arr[4]=new student();
+    student *arr[5];
+    arr[0] = new student();
+    arr[1] = new student();
+    arr[2] = new student();
+    arr[3] = new student();
+    arr[4] = new student();
 
-    for(int i = 0; i<5; i++)
+    for (int i = 0; i < 5; i++)
     {
         arr[i]->acceptStudent();
     }
-    cout<<"********************************************************"<<endl;
-    cout<<"Here are the Students details: "<<endl<<endl;
-    for(int i = 0; i<5; i++)
+    cout << "********************************************************" << endl;
+
+    cout << "Here are the Students details: " << endl;
+
+    for (int i = 0; i < 5; i++)
     {
         arr[i]->printStudent();
     }
-
 }
