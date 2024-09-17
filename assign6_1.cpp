e6f96bce0188d9f4bf2f6ff3222dd3e925e7c28e
86729-harshada <harshada3678@gmail.com>
2024-09-18 05:06:58 +0530

    updated

--- assign6_1.cpp
+++ assign6_1.cpp
@@ -10,16 +10,24 @@ class Date
 public:
     Date()
     {
-        day;
-        month;
-        year;
-        dob;
+        this->day=0;
+        this->month=0;
+        this->year=0;
     }
     Date(int day, int month, int year)
     {
         this->day = day;
         this->month = month;
         this->year = year;
+    }
+    void acceptDate()
+    {
+        //cout << "Enter the day, month, year " << endl;
+        cin >> day >> month >> year;
+    }
+    void displayDate()
+    {
+        cout<<day<<"/"<<month<<"/"<<year<<endl;
     }
 };
 
@@ -28,6 +36,8 @@ class Employee
     int id;
     double sal;
     string dep;
+    Date *DOB;
+    Date DOJ;
 
 public:
     Employee()
@@ -35,22 +45,68 @@ public:
         id = 0;
         sal = 0;
         dep = " ";
+        DOB = NULL;
     }
-    Employee(int id, double sal, string dept)
+    Employee(int id, double sal, string dept, int day, int month, int year):DOJ(day,month,year)
     {
         this->id = id;
         this->sal = sal;
         this->dep = dep;
+        DOB=NULL;
     }
-    void display()
+    void acceptEmp()
     {
         cout << "Enter the details : " << endl;
-        cout << " : " << endl;
+        cout << " id : ";
+        cin>>id;
+        cout << " sal : ";
+        cin>>sal;
+        cout << " dept : ";
+        cin>>dep;
+        cout<<" date of joining : ";
+        DOJ.acceptDate();
+    }
+    void displayEmp()
+    {
+        cout << "---------------Emp details----------------" << endl;
+        cout << "emp id =  "<<id <<endl <<"Sal = "<<sal <<endl << "department = " <<dep <<endl << "date of joining : ";
+        DOJ.displayDate();
+        if(DOB != NULL)
+        {
+            cout<<"Date of Birth = ";
+            DOB->displayDate();
+        }
+    }
+
+    void acceptDOB()
+    {
+        DOB = new Date();
+        cout<<"Enter the Date of Birth : ";
+        DOB->acceptDate();
+    }
+
+    ~Employee()
+    {
+        if(DOB != NULL)
+        {
+            delete DOB;
+            DOB = NULL;
+        }
     }
 };
 
 int main()
 {
     Employee e1;
-    e1.display();
+    e1.acceptEmp();
+    e1.acceptDOB();
+    cout<<"**********************************************************"<<endl;
+
+    Employee e2;
+    e2.acceptEmp();
+    e2.acceptDOB();
+
+    e1.displayEmp();
+    
+    e2.displayEmp();
 }
